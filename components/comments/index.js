c4ce9b6301866eef5d41c4b3acd6c4835a9f7e51
bulhwi <mm225012@gmail.com>
2023-08-05 21:25:40 +0900

    댓글 영역 주석 처리

--- components/comments/index.js
+++ components/comments/index.js
@@ -24,15 +24,16 @@ const Comments = ({ frontMatter }) => {
   const comment = siteMetadata?.comment
   if (!comment || Object.keys(comment).length === 0) return <></>
   return (
-    <div id="comment">
-      {siteMetadata.comment && siteMetadata.comment.provider === 'giscus' && <GiscusComponent />}
-      {siteMetadata.comment && siteMetadata.comment.provider === 'utterances' && (
-        <UtterancesComponent />
-      )}
-      {siteMetadata.comment && siteMetadata.comment.provider === 'disqus' && (
-        <DisqusComponent frontMatter={frontMatter} />
-      )}
-    </div>
+    <div></div>
+    // <div id="comment">
+    //   {siteMetadata.comment && siteMetadata.comment.provider === 'giscus' && <GiscusComponent />}
+    //   {siteMetadata.comment && siteMetadata.comment.provider === 'utterances' && (
+    //     <UtterancesComponent />
+    //   )}
+    //   {siteMetadata.comment && siteMetadata.comment.provider === 'disqus' && (
+    //     <DisqusComponent frontMatter={frontMatter} />
+    //   )}
+    // </div>
   )
 }
 
